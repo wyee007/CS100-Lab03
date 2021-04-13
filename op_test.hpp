6fb1c26f064e95edfce37c403dc60970871ea973
Devotrix <jchan443@ucr.edu>
2021-04-13 16:26:13 -0700

    added stringifytest

--- op_test.hpp
+++ op_test.hpp
@@ -10,4 +10,8 @@ TEST(OpTest, OpEvaluateNonZero) {
     EXPECT_EQ(test->evaluate(), 8);
 }
 
+TEST(OpTest, OpStringifyNonZero){
+   Op* test = new Op(8);
+   EXPECT_EQ(test->stringify(), "8.000000");
+}
 #endif //__OP_TEST_HPP__
