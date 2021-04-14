aa1ee474c9528d70a5da74183a9f1370cd6066f1
NateGaNe <81611522+NateGaNe@users.noreply.github.com>
2021-04-13 17:57:21 -0700

    Update mult_test.hpp

--- mult_test.hpp
+++ mult_test.hpp
@@ -15,6 +15,6 @@ TEST(MultTest, MultStringifyTwoNonZero){
 	Op* five = new Op(5);
 	Op* six = new Op(6);
 	Mult* test = new Mult(five, six);
-	EXPECT_EQ(test->stringify(), "(5.000000*6.000000)");
+	EXPECT_EQ(test->stringify(), (5.000000*6.000000));
 }
 #endif //__MULT_TEST_HPP__
