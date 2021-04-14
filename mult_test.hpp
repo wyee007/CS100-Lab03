edf59e3101083ccdb2a6ae6205de7cc868ed0e6e
Devotrix <jchan443@ucr.edu>
2021-04-14 15:11:55 -0700

    Added more test to mult_test

--- mult_test.hpp
+++ mult_test.hpp
@@ -17,4 +17,18 @@ TEST(MultTest, MultStringifyTwoNonZero){
 	Mult* test = new Mult(five, six);
 	EXPECT_EQ(test->stringify(), "(5.000000*6.000000)");
 }
+TEST(MultTest, MultTestwOneZero){
+	Op* five = new Op(5);
+	Op* zero = new Op(0);
+	Mult* test = new Mult(five, zero);
+	EXPECT_EQ(test->stringify(), 0);
+}
+TEST(MultTest, MultDoubleTest){
+	Op* five = new Op(5);
+	Op* six = new Op(6);
+	Op* ten = new Op(10);
+	Mult* testOne = new Mult(five, six);
+	Mult* testTwo = new Mult(testOne, ten);
+	EXPECT_EQ(testTwo->stringify(), 300);
+}
 #endif //__MULT_TEST_HPP__
