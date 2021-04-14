591b285135ad61821ce2fe2f2fa06a7bb3067da7
NateGaNe <81611522+NateGaNe@users.noreply.github.com>
2021-04-13 17:57:45 -0700

    Update div_test.hpp

--- div_test.hpp
+++ div_test.hpp
@@ -16,7 +16,7 @@ TEST(DivTest, DivStringifyWithNonZero){
 	Op* thirtyNine = new Op(39);
 	Op* thirteen = new Op(13);
 	Div* test = new Div(thirtyNine, thirteen);
-	EXPECT_EQ(test->stringify(), "(39.000000/13.000000)");
+	EXPECT_EQ(test->stringify(), (39.000000/13.000000));
 }
 #endif //__DIV_TEST_HPP__
 
