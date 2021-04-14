79cce9adcc5d167264f72f9d78bffc5d53a9d600
NateGaNe <81611522+NateGaNe@users.noreply.github.com>
2021-04-13 17:21:17 -0700

    Update op_test.hpp

--- op_test.hpp
+++ op_test.hpp
@@ -14,4 +14,12 @@ TEST(OpTest, OpStringifyNonZero){
    Op* test = new Op(8);
    EXPECT_EQ(test->stringify(), "8.000000");
 }
+TEST(OpTest, OpStringifyZero){
+   Op* test = new Op(0);
+   EXPECT_EQ(test->stringify(), "0");
+}
+TEST(OpTest, OpStringifyNegative){
+   Op* test = new Op(-1);
+   EXPECT_EQ(test->stringify(), -1);
+}
 #endif //__OP_TEST_HPP__
